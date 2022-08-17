a5ffd17157979015100ce4abf4e060d30524624a
Udaykiran340 <kironagk@gmail.com>
2022-08-17 18:05:08 +0530

    Update todoitem.cs

--- Models/todoitem.cs
+++ Models/todoitem.cs
@@ -1,8 +1,14 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace W1.Models{
     public class todoitem{
-        [JsonIgnore]
         public int id{get;set;}
-        public item i1{get;set;}
+        public int itemid{get;set;}
+    public todoitem.item _item { get; set; }
+    public class item{
+            public int itemid{get;set;}
+            public string desc{get;set;}
+        }
     }
+
 }
