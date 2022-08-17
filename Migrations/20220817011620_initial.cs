3404d483ff7a33c63c2769b6f740ee6c73fc7e0d
Udaykiran340 <kironagk@gmail.com>
2022-08-17 06:51:31 +0530

    Create 20220817011620_initial.cs

---
+++ Migrations/20220817011620_initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace W1.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "item",
+                columns: table => new
+                {
+                    itemid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    desc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_item", x => x.itemid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    i1itemid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_TodoItems_item_i1itemid",
+                        column: x => x.i1itemid,
+                        principalTable: "item",
+                        principalColumn: "itemid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_i1itemid",
+                table: "TodoItems",
+                column: "i1itemid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "item");
+        }
+    }
+}
